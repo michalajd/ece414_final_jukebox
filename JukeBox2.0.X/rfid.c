e581bf83a5994724833fd66bb996011f4130ecba
arnolddj1 <arnolddj@lafayette.edu>
2022-12-11 00:46:23 -0500

    switch added, 1-3 work, others don't

--- JukeBox2.0.X/rfid.c
+++ JukeBox2.0.X/rfid.c
@@ -21,7 +21,7 @@ int concat_int(int a, int b){
     
 }
 
-void readTag(){
+void readTag(int num){
     
     switch (num){
         case 0x5A12:
@@ -44,13 +44,472 @@ void readTag(){
             
         case 0x5853:
             uart1_txwrite(0xAA);
-               uart1_txwrite(0x07);
-               uart1_txwrite(0x02);
-               uart1_txwrite(0x00);
-               uart1_txwrite(0x03);
-               uart1_txwrite(0xB6);
+            uart1_txwrite(0x07);
+            uart1_txwrite(0x02);
+            uart1_txwrite(0x00);
+            uart1_txwrite(0x03);
+            uart1_txwrite(0xB6);
             break;
-                
+            
+        case 0x58AC:
+            uart1_txwrite(0xAA);
+            uart1_txwrite(0x07);
+            uart1_txwrite(0x02);
+            uart1_txwrite(0x00);
+            uart1_txwrite(0x04);
+            uart1_txwrite(0xB7);
+            break;
+            
+        case 0x599C:
+            uart1_txwrite(0xAA);
+            uart1_txwrite(0x07);
+            uart1_txwrite(0x02);
+            uart1_txwrite(0x00);
+            uart1_txwrite(0x05);
+            uart1_txwrite(0xB8);
+            break;
+        
+            case 0x57AA:
+            uart1_txwrite(0xAA);
+            uart1_txwrite(0x07);
+            uart1_txwrite(0x02);
+            uart1_txwrite(0x00);
+            uart1_txwrite(0x06);
+            uart1_txwrite(0xB9);
+            break;
+            
+            case 0x47BB:
+            uart1_txwrite(0xAA);
+            uart1_txwrite(0x07);
+            uart1_txwrite(0x02);
+            uart1_txwrite(0x00);
+            uart1_txwrite(0x07);
+            uart1_txwrite(0xBA);
+            break;
+            
+            case 0x4D47:
+            uart1_txwrite(0xAA);
+            uart1_txwrite(0x07);
+            uart1_txwrite(0x02);
+            uart1_txwrite(0x00);
+            uart1_txwrite(0x08);
+            uart1_txwrite(0xBB);
+            break;
+            
+            case 0x7737:
+            uart1_txwrite(0xAA);
+            uart1_txwrite(0x07);
+            uart1_txwrite(0x02);
+            uart1_txwrite(0x00);
+            uart1_txwrite(0x09);
+            uart1_txwrite(0xBC);
+            break;
+            
+            case 0x47CC: // 10
+            uart1_txwrite(0xAA);
+            uart1_txwrite(0x07);
+            uart1_txwrite(0x02);
+            uart1_txwrite(0x00);
+            uart1_txwrite(0x0A);
+            uart1_txwrite(0xBD);
+            break;
+            
+            case 0x3608:
+            uart1_txwrite(0xAA);
+            uart1_txwrite(0x07);
+            uart1_txwrite(0x02);
+            uart1_txwrite(0x00);
+            uart1_txwrite(0x0B);
+            uart1_txwrite(0xBE);
+            break;
+            
+            case 0x5F68:
+            uart1_txwrite(0xAA);
+            uart1_txwrite(0x07);
+            uart1_txwrite(0x02);
+            uart1_txwrite(0x00);
+            uart1_txwrite(0x0C);
+            uart1_txwrite(0xBF);
+            break;
+            
+            case 0x2B78:
+            uart1_txwrite(0xAA);
+            uart1_txwrite(0x07);
+            uart1_txwrite(0x02);
+            uart1_txwrite(0x00);
+            uart1_txwrite(0x0D);
+            uart1_txwrite(0xC0);
+            break;
+            
+            case 0x5928:
+            uart1_txwrite(0xAA);
+            uart1_txwrite(0x07);
+            uart1_txwrite(0x02);
+            uart1_txwrite(0x00);
+            uart1_txwrite(0x0E);
+            uart1_txwrite(0xC1);
+            break;
+            
+            case 0x59D0:
+            uart1_txwrite(0xAA);
+            uart1_txwrite(0x07);
+            uart1_txwrite(0x02);
+            uart1_txwrite(0x00);
+            uart1_txwrite(0x0F);
+            uart1_txwrite(0xC2);
+            break;
+            
+            case 0x3426:
+            uart1_txwrite(0xAA);
+            uart1_txwrite(0x07);
+            uart1_txwrite(0x02);
+            uart1_txwrite(0x00);
+            uart1_txwrite(0x10);
+            uart1_txwrite(0xC3);
+            break;
+            
+            case 0x2544:
+            uart1_txwrite(0xAA);
+            uart1_txwrite(0x07);
+            uart1_txwrite(0x02);
+            uart1_txwrite(0x00);
+            uart1_txwrite(0x11);
+            uart1_txwrite(0xC4);
+            break;
+            
+            case 0x33BE:
+            uart1_txwrite(0xAA);
+            uart1_txwrite(0x07);
+            uart1_txwrite(0x02);
+            uart1_txwrite(0x00);
+            uart1_txwrite(0x12);
+            uart1_txwrite(0xC5);
+            break;
+            
+            case 0x2276:
+            uart1_txwrite(0xAA);
+            uart1_txwrite(0x07);
+            uart1_txwrite(0x02);
+            uart1_txwrite(0x00);
+            uart1_txwrite(0x13);
+            uart1_txwrite(0xC6);
+            break;
+            
+            case 0x5DBE: // 20
+            uart1_txwrite(0xAA);
+            uart1_txwrite(0x07);
+            uart1_txwrite(0x02);
+            uart1_txwrite(0x00);
+            uart1_txwrite(0x14);
+            uart1_txwrite(0xC7);
+            break;
+            
+            case 0x2B0A:
+            
+            break;
+            
+            case 0x8383:
+            
+            break;
+            
+            case 0x5ADB:
+            
+            break;
+            
+            case 0x25F0:
+            
+            break;
+            
+            case 0x5668:
+            
+            break;
+            
+            case 0x4E0B:
+            
+            break;
+            
+            case 0x255B:
+            
+            break;
+            
+            case 0x458F:
+            
+            break;
+            
+            case 0x83F2: 
+            
+            break;
+            
+            case 0x5B2F: // 30
+            
+            break;
+            
+            case 0x56D7:
+            
+            break;
+            
+            case 0x2A86:
+            
+            break;
+            
+            case 0x84B5:
+            
+            break;
+            
+            case 0x470F:
+            
+            break;
+            
+            case 0x28C6:
+            
+            break;
+            
+            case 0x225B:
+            
+            break;
+            
+            case 0x2236:
+            
+            break;
+            
+            case 0x5D91:
+            
+            break;
+            
+            case 0x5B24:
+            
+            break;
+            
+            case 0x8368: // 40
+            
+            break;
+            
+            case 0x773E:
+            
+            break;
+            
+            case 0x3650:
+            
+            break;
+            
+            case 0x4824:
+            
+            break;
+            
+            case 0x1BB7:
+            
+            break;
+            
+            case 0x59C3:
+            
+            break;
+            
+            case 0x57BF:
+            
+            break;
+            
+            case 0x57DA:
+            
+            break;
+            
+            case 0x5870:
+            
+            break;
+            
+            case 0x5732:
+            
+            break;
+            
+            case 0x2AC7: // 50
+            
+            break;
+            
+            case 0x2560:
+            
+            break;
+            
+            case 0x5ECB:
+            
+            break;
+            
+            case 0x4DD2:
+            
+            break;
+            
+            case 0x5CFE:
+            
+            break;
+            
+            case 0x5B05:
+            
+            break;
+            
+            case 0x36A9:
+            
+            break;
+            
+            case 0x2AD5:
+            
+            break;
+            
+            case 0x65EF:
+            
+            break;
+            
+            case 0x2886:
+            
+            break;
+            
+            case 0x2219: // 60
+            
+            break;
+            
+            case 0x44A7:
+            
+            break;
+            
+            case 0x47B2:
+            
+            break;
+            
+            case 0x58AF:
+            
+            break;
+            
+            case 0x32FE:
+            
+            break;
+            
+            case 0x5B91:
+            
+            break;
+            
+            case 0x2A67:
+            
+            break;
+            
+            case 0x218A:
+            
+            break;
+            
+            case 0x330E:
+            
+            break;
+            
+            case 0x2A9F:
+            
+            break;
+            
+            case 0x826A: // 70
+            
+            break;
+            
+            case 0x57E3:
+            
+            break;
+            
+            case 0x5AFE:
+            
+            break;
+            
+            case 0x462E:
+            
+            break;
+            
+            case 0x5B09:
+            
+            break;
+            
+            case 0x2ADD:
+            
+            break;
+            
+            case 0x2552:
+            
+            break;
+            
+            case 0x8365:
+            
+            break;
+            
+            case 0x7593:
+            
+            break;
+            
+            case 0x4E42: 
+            
+            break;
+            
+            case 0x4D80: // 80
+            
+            break;
+            
+            case 0x56F6:
+            
+            break;
+            
+            case 0x463A:
+            
+            break;
+            
+            case 0x6AFE:
+            
+            break;
+            
+            case 0x2617:
+            
+            break;
+            
+            case 0x4E5F:
+            
+            break;
+            
+            case 0x5ABA:
+            
+            break;
+            
+            case 0x2F9E:
+            
+            break;
+            
+            case 0x4B76:
+            
+            break;
+            
+            case 0x831C:
+            
+            break;
+            
+            case 0x4DF3:
+            
+            break;
+            
+            case 0x57F5:
+            
+            break;
+            
+            case 0x5CF8:
+            
+            break;
+            
+            case 0x5B8A:
+            
+            break;
+            
+            case 0xAE1B:
+            
+            break;
+            
+            case 0x4648:
+            
+            break;
+            
+        default:
+            
+            
+            
+            break;
+            
     }
     
 }
--- JukeBox2.0.X/testmain.c
+++ JukeBox2.0.X/testmain.c
@@ -26,7 +26,7 @@ int buffer[RX_BUFFER];
 uint8_t i = 0;
 int num; // concatenated hex number
 
-int main(int argc, char** argv) {
+int main() {
     uart1_init(9600);
     
     while(1){
@@ -37,7 +37,7 @@ int main(int argc, char** argv) {
                 buffer[i] = tmp;
                 i++;
 
-            if (i>9) {
+            if (i > 9) {
                 i = 0;
                 //temp_buff = buffer[5]; // temp char to store tag ID for tag())
                 //uart1_txwrite(buffer[5]); // uncomment this to see first two hex numbers of tag ID (also comment out all other uart writes)
@@ -45,53 +45,13 @@ int main(int argc, char** argv) {
                 
                 num = concat_int(buffer[5], buffer[6]); 
             
-                readTag();
-            
-                
-             
-               else if(num == 0x1AE9){
-               uart1_txwrite(0xAA);
-               uart1_txwrite(0x07);
-               uart1_txwrite(0x02);
-               uart1_txwrite(0x00);
-               uart1_txwrite(0x02);
-               uart1_txwrite(0xB5);
-                }
-                else if(buffer[5] == 0x58 && buffer[6] == 0x53){
-               uart1_txwrite(0xAA);
-               uart1_txwrite(0x07);
-               uart1_txwrite(0x02);
-               uart1_txwrite(0x00);
-               uart1_txwrite(0x03);
-               uart1_txwrite(0xB6);
-                }
-                else if(buffer[5] == 0x58 && buffer[6] == 0xAC){
-              /* uart1_txwrite(0xAA);
-               uart1_txwrite(0x07);
-               uart1_txwrite(0x02);
-               uart1_txwrite(0x00);
-               uart1_txwrite(0x04);
-               uart1_txwrite(0xB7);*/
-               uart1_txwrite(0xAA);
-               uart1_txwrite(0x06);
-               uart1_txwrite(0x00);
-               uart1_txwrite(0xB0);
-                }
-                else if(buffer[5] == 0x59 && buffer[6] == 0x9C){
-               uart1_txwrite(0xAA);
-               uart1_txwrite(0x07);
-               uart1_txwrite(0x02);
-               uart1_txwrite(0x00);
-               uart1_txwrite(0x05);
-               uart1_txwrite(0xB8);
-                }
+                readTag(num); // switch statement that checks each tag ID
+              
             }
         }
-
                 //tag_flag = tag_read_bool(); // sets tag flag to 1 while input is reading
         
-        } //end of while(1)
-
+    } //end of while(1)
         
         //tag(temp_buff);
         //FSM_tick();
